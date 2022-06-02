2fcab507609dfac80905804b4dfc18d21e827131
temu-05 <99035848+temu-05@users.noreply.github.com>
2022-06-02 22:47:30 +0300

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-from sys import argv
-
-
 if __name__ == "__main__":
-    arglen = len(argv) - 1
-    if arglen == 0:
-        print('0 arguments.')
-    elif arglen == 1:
-        print('1 argument:')
-        print('1: {}'.format(argv[1]))
+    """Print the number of and list of arguments."""
+    import sys
+
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
     else:
-        print('{} arguments:'.format(arglen))
-        for i in range(1, len(argv)):
-            print('{}: {}'.format(i, argv[i]))
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
